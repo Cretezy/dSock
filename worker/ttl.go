c9fe01268f96a1f75d9a002c329d5297f265e87c
Charles Crete <charles@cretezy.com>
2021-04-11 12:47:39 -0400

    Make TTL refresh run every duration

--- worker/ttl.go
+++ worker/ttl.go
@@ -8,24 +8,37 @@ import (
 )
 
 func RefreshTtls() {
-	_, err := redisClient.Pipelined(func(pipeliner redis.Pipeliner) error {
-		redisWorker := map[string]interface{}{
-			"lastPing": time.Now().Format(time.RFC3339),
+	nextTime := time.Now()
+
+	for {
+		nextTime = nextTime.Add(options.TtlDuration)
+		time.Sleep(time.Until(nextTime))
+
+		_, err := redisClient.Pipelined(func(pipeliner redis.Pipeliner) error {
+			redisWorker := map[string]interface{}{
+				"lastPing": time.Now().Format(time.RFC3339),
+			}
+
+			pipeliner.HSet("worker:"+workerId, redisWorker)
+			pipeliner.Expire("worker:"+workerId, options.TtlDuration+common.TtlBuffer)
+
+			for connId := range connections.state {
+				pipeliner.Expire("conn:"+connId, options.TtlDuration+common.TtlBuffer)
+			}
+
+			return nil
+		})
+
+		if err != nil {
+			logger.Error("Could not refresh TTLs",
+				zap.Error(err),
+				zap.String("workerId", workerId),
+			)
+
+			continue
 		}
 
-		pipeliner.HSet("worker:"+workerId, redisWorker)
-		pipeliner.Expire("worker:"+workerId, options.TtlDuration+common.TtlBuffer)
-
-		for connId := range connections.state {
-			pipeliner.Expire("conn:"+connId, options.TtlDuration+common.TtlBuffer)
-		}
-
-		return nil
-	})
-
-	if err != nil {
-		logger.Error("Could not refresh TTLs",
-			zap.Error(err),
+		logger.Info("Refreshed TTLs",
 			zap.String("workerId", workerId),
 		)
 	}
